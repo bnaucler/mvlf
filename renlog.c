bc7f561520496f42e30435f0081f66bfa650afa0
bnaucler <mail@bnaucler.se>
2016-12-25 15:37:44 +0100

    Renaming project (again)

--- renlog.c
+++ renlog.c
@@ -8,28 +8,44 @@
 #define NPREF "ljusdal.log."
 
 #define ERRSTR "ERROR"
+#define DDIV '/'
 
 #define DLEN 2
 #define MLEN 3
 #define YLEN 4
 
-#define FNCH 128
 #define FPCH 256
 #define SBCH 32
 
-char m[12][4] = {
+char ms[12][4] = {
 	"jan", "feb", "mar", "apr",
 	"may", "jun", "jul", "aug",
 	"sep", "oct", "nov", "dec"
 };
 
+char ml[12][10] = {
+	"january", "february", "march", "april",
+	"may", "june", "july", "august",
+	"september", "october", "november", "december"
+};
+
+char ds[7][4] = {
+	"mon", "tue", "wed", "thu",
+	"fri", "sat", "sun"
+}
+
+char dl[7][10] = {
+	"monday", "tuesday", "wednesday", "thursday",
+	"friday", "saturday", "sunday"
+}
+
 int mnum(char *txmon) {
 
 	int a = 0;
-	int alen = sizeof(m) / sizeof(m[0]);
+	int alen = sizeof(ms) / sizeof(ms[0]);
 
 	for (a = 0; a < alen; a++) {
-		if (strcasecmp(txmon, m[a]) == 0) return a + 1;
+		if (strcasecmp(txmon, ms[a]) == 0) return a + 1;
 	}
 
 	return 0;
@@ -42,7 +58,7 @@ char *mknn(char *oname, char *nname) {
 
 	if (strlen(oname) != oplen + DLEN + MLEN + YLEN) return ERRSTR;
 
-	memset(nname, 0, FNCH);
+	memset(nname, 0, FPCH);
 
 	int a = 0;
 	int y = 0, m = 2, d = 0;
@@ -58,8 +74,8 @@ char *mknn(char *oname, char *nname) {
 	for (a = 0; a < YLEN; a++) { buf[a] = oname[(oplen + DLEN + MLEN + a)]; }
 	y = atoi(buf);
 
-	if (d == 0 || m == 0 || y == 0) snprintf(nname, FNCH, ERRSTR);
-	else snprintf(nname, FNCH, "%s%04d%02d%02d", NPREF, y, m, d);
+	if (d == 0 || m == 0 || y == 0) snprintf(nname, FPCH, ERRSTR);
+	else snprintf(nname, FPCH, "%s%04d%02d%02d", NPREF, y, m, d);
 
 	free(buf);
 	return nname;
@@ -68,6 +84,7 @@ char *mknn(char *oname, char *nname) {
 int usage(char *cmd, char *err, int ret) {
 
 	if (strlen(err) > 0) printf("ERROR: %s\n", err);
+	printf("");
 	printf("Usage: %s [dir]\n", cmd);
 
 	exit(ret);
@@ -79,23 +96,34 @@ int main(int argc, char *argv[]) {
 	struct dirent *dir;
 
 	char *dpath = calloc(FPCH, sizeof(char));
-	char *nname = calloc(FNCH, sizeof(char));
+	char *oname = calloc(FPCH, sizeof(char));
+	char *nname = calloc(FPCH, sizeof(char));
+	char *buf = calloc(FPCH, sizeof(char));
 
 	unsigned int a = 0;
 	int plen = strlen(OPREF);
 
-	if (argc > 2) usage(argv[0], "", 1);
+	while((optc = getopt(argc, argv, "d:i:o:p:f:")) != -1) {
+		switch (optc) {
+
+			default:
+				usage(argv[0], "", 0);
+				break;
+		}
+	}
+
+	if (argc > 2) usage(argv[0], "Too many arguments", 1);
 	else if (argc == 1) dpath = getcwd(dpath, FPCH);
 	else {
-		if (strlen(argv[1]) > FPCH) {
+		if (strlen(argv[1]) > (FPCH + plen + YLEN + MLEN + DLEN + 1)) {
 			usage(argv[0], "Your directory paths are messed up", 1);
 		} else {
 			strcpy(dpath, argv[1]);
 		}
 	}
 
+	if (dpath[strlen(dpath) - 1] != DDIV) dpath[strlen(dpath)] = DDIV;
 	d = opendir(dpath);
-	if (dpath[strlen(dpath) - 1] != '/') dpath[strlen(dpath)] = '/';
 
 	if (!d) {
 		usage(argv[0], "Could not read directory", 1);
@@ -106,13 +134,13 @@ int main(int argc, char *argv[]) {
 			for (a = 0; a < plen; a++) {
 				if (dir->d_name[a] != OPREF[a]) break;
 				if (a == plen - 1) {
-					nname = mknn(dir->d_name, nname);
+					snprintf(oname, FPCH, "%s%s", dpath, dir->d_name);
+					snprintf(nname, FPCH, "%s%s", dpath, mknn(dir->d_name, buf));
 					if (strcasecmp(nname, ERRSTR) == 0) {
-						// printf("Error: could not rename %s\n", dir->d_name);
+						printf("Error: could not rename %s\n", oname);
 					} else {
 						// rename(dir->d_name, nname);
-						printf("%s%s\t%s%s\n", 
-								dpath, dir->d_name, dpath, nname);
+						printf("%s\t%s\n", oname, nname);
 					}
 				}
 			}
